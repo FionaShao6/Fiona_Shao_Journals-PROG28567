30383ab151157b7107546519190a0f640ed8f10b
FionaShao6 <fionashao6@gmail.com>
2025-09-22 14:00:09 -0400

    Week3 Task 2A – Enemy Motion Planning

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,13 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using PlasticPipe.PlasticProtocol.Messages;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform playerTransform;
+    public float yOffset;
+    public float xOffset;
+    public float moveSpeed = 3f;
+    
     private void Update()
     {
+
+        EnemyMovement();
+    }
+    public Vector2 CalculateTargetPosition()
+    {
+        float targetX = playerTransform.position.x + xOffset;
+        float targetY = playerTransform.position.y + yOffset;
+        return new Vector2(targetX, targetY);
+    }
+    public void MoveToTarget(Vector3 target)
+    {
+        // Calculate the direction and distance from the enemy's current position to the target position
+        Vector3 directionToTarget = target - transform.position;
+
+        Vector3 moveStep = directionToTarget.normalized * moveSpeed * Time.deltaTime;
+  
+        transform.position += moveStep;
     }
     public void EnemyMovement()
     {
-
+        if (playerTransform == null)
+        {
+            return;
+        }
+        Vector2 targetPosition = CalculateTargetPosition();
+        MoveToTarget(targetPosition);
     }
+
+
+   
 }
